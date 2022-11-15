10ba54e7f35299d294dc2f9506174dddaca5032c
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2022-11-15 11:15:26 +0100

    fix non-update bug

--- make_arduino_library.sh
+++ make_arduino_library.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 cd arduino
-rm -r syndesi
+rm -r syndesi syndesi.zip
 mkdir syndesi
-mkdir syndesi/include
-mkdir syndesi/src
-mkdir syndesi/user_config
 mkdir syndesi/examples
 
 touch syndesi/keywords.txt
@@ -12,11 +9,10 @@ touch syndesi/keywords.txt
 touch syndesi/examples/test.ino
 
 
-cp ../include/*.h syndesi
-cp ../src/*.cpp syndesi
-#cp ../user_config/*.h syndesi
+cp ../include/*.h syndesi -f
+cp ../src/*.cpp syndesi -f
 
 # Copy examples
-cp ../arduino_examples/* examples -r
+cp ../arduino_examples/* examples -r -f
 
 zip -r syndesi.zip syndesi -q
