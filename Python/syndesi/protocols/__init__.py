b01489a633644db25dd64dde32347463a15b588c
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-07-21 07:38:42 +0200

    Add protocols to __init__ for easy import

--- Python/syndesi/protocols/__init__.py
+++ Python/syndesi/protocols/__init__.py
@@ -1,1 +1,6 @@
 from syndesi.protocols.iprotocol import IProtocol
+from .commands import RawCommands
+from .raw_stream import RawStream
+from .raw import Raw
+from .scpi import SCPI
+from .sdp import SDP
