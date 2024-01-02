733099f9d375af19f07d2b1dfccba8e02b7f358e
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2024-01-02 16:16:01 +0100

    Fix IAdapter bug

--- Python/syndesi/adapters/iadapter.py
+++ Python/syndesi/adapters/iadapter.py
@@ -55,12 +55,12 @@ class IAdapter(ABC):
         # not used because of termination or length stop condition
         self._previous_read_buffer = b''
 
-    @abstractmethod
     def flushRead(self):
         """
         Flush the input buffer
         """
         self._read_queue.clear()
+        self._previous_read_buffer = b''
 
     @abstractmethod
     def open(self):
@@ -105,41 +105,41 @@ class IAdapter(ABC):
         stop_condition : StopCondition or None
             Set a custom stop condition, if None (Default), the adapater stop condition is used
         """
+        # Use adapter values if no custom value is specified
         if timeout is None:
-            read_timeout = self._timeout
-        else:
-            read_timeout = timeout
+            timeout = self._timeout
         
-        if stop_condition is not None:
-            
+        if stop_condition is None:
+            stop_condition = self._stop_condition
 
-
+        # If the adapter is closed, open it
         if self._status == self.Status.DISCONNECTED:
             self.open()
 
         self._start_thread()
 
-        timeout = self._timeout.initiate_read(len(self._previous_read_buffer) > 0)
-        if self._stop_condition is not None:
-            self._stop_condition.initiate_read()
+        timeout_ms = timeout.initiate_read(len(self._previous_read_buffer) > 0)
+
+        if stop_condition is not None:
+            stop_condition.initiate_read()
 
         deferred_buffer = b''
 
         # Start with the deferred buffer
-        if len(self._previous_read_buffer) > 0:
-            stop, output, self._previous_read_buffer = self._stop_condition.evaluate(self._previous_read_buffer)
+        if len(self._previous_read_buffer) > 0 and stop_condition is not None:
+            stop, output, self._previous_read_buffer = stop_condition.evaluate(self._previous_read_buffer)
         else:
             stop = False
             output = b''
         # If everything is used up, read the queue
         if not stop:
             while True:
-                (timestamp, fragment) = self._read_queue.get(timeout)
+                (timestamp, fragment) = self._read_queue.get(timeout_ms)
 
                 # 1) Evaluate the timeout
-                stop, timeout = self._timeout.evaluate(timestamp)
+                stop, timeout_ms = timeout.evaluate(timestamp)
                 if stop:
-                    data_strategy, origin = self._timeout.dataStrategy()
+                    data_strategy, origin = timeout.dataStrategy()
                     if data_strategy == Timeout.OnTimeoutStrategy.DISCARD:
                         # Trash everything
                         output = b''
@@ -160,9 +160,10 @@ class IAdapter(ABC):
                 # Add the deferred buffer
                 if len(deferred_buffer) > 0:
                     fragment = deferred_buffer + fragment
+
                 # 2) Evaluate the stop condition
-                if self._stop_condition is not None:
-                    stop, kept_fragment, deferred_buffer = self._stop_condition.evaluate(fragment)
+                if stop_condition is not None:
+                    stop, kept_fragment, deferred_buffer = stop_condition.evaluate(fragment)
                     output += kept_fragment
                     if stop:
                         self._previous_read_buffer = deferred_buffer
