35af59a81914baba2b1b54c91c00e5184551eb8b
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2025-11-25 18:35:29 +0100

    Update syndesi-backend

--- syndesi/scripts/syndesi_backend.py
+++ syndesi/scripts/syndesi_backend.py
@@ -3,43 +3,45 @@
 # License : GPL
 
 import argparse
+from enum import StrEnum
 
 from ..cli.backend_console import BackendConsole
 from ..cli.backend_status import BackendStatus
 #from ..cli.backend_wrapper import BackendWrapper
 from ..adapters.backend.backend import main as backend_main
 
+class Command(StrEnum):
+    STATUS = 'status'
+    CONSOLE = 'console'
+    START = 'start'
 
 def main() -> None:
-    argument_parser = argparse.ArgumentParser(prog="syndesi-backend")
+    argument_parser = argparse.ArgumentParser(prog="syndesi-backend", add_help=False)
+    
 
     argument_parser.add_argument(
-        "--status", action="store_true", help="Show backend status"
-    )
-
-    argument_parser.add_argument(
-        "--console", action="store_true", help="Run backend console"
-    )
-
-    argument_parser.add_argument(
-        "--start", action="store_true", help="Start the backend"
+        "command",
+        nargs="?",
+        choices=list(Command),
+        help='Command to run'
     )
 
     args, remaining_args = argument_parser.parse_known_args()
 
-    print(args)
+    if args.command is None:
+        argument_parser.print_help()
+        return
 
-    if args.status:
+    if args.command == Command.STATUS:
         status = BackendStatus(remaining_args)
         status.run()
-    elif args.console:
+    elif args.command == Command.CONSOLE:
         console = BackendConsole(remaining_args)
         console.run()
-    else:
+    elif args.command == Command.START:
         backend_main(remaining_args)
-        #backend = BackendWrapper(remaining_args)
-        #backend.run()
-TODO : Make this work without the wrapper and make clean so as to have a help for the syndesi-backend command and also get help for the backend itself (--address, --port, etc...)
+    else:
+        print(f'Invalid command : {args.command}')
 
 
 if __name__ == "__main__":
